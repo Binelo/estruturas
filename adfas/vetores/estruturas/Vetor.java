bd72de9cae4a02ebcd1479f7a0f8d73ecb4efcee
João Pedro Binelo <112009925+Binelo@users.noreply.github.com>
2025-03-07 21:53:48 -0300

    Add files via upload

--- adfas/vetores/estruturas/Vetor.java
+++ adfas/vetores/estruturas/Vetor.java
@@ -14,12 +14,15 @@ public class Vetor {
 
     public void adicionar(Integer valor) {
         if (this.tamanho == this.capacidade) {
-            System.out.println("não adicionarás");
-            return;
-        } else {
-            this.elementos[this.tamanho] = valor;
-            this.tamanho++;
-        }
+            Integer[] temp = this.elementos;
+            this.capacidade = this.capacidade * 2;
+            this.elementos = new Integer[capacidade];
+            for (int i = 0; i < this.tamanho; i++) {
+                this.elementos[i] = temp[i];
+            }
+        } 
+        this.elementos[this.tamanho] = valor;
+        this.tamanho++;
     }
 
     public void mostrarElementos() {
@@ -27,6 +30,7 @@ public class Vetor {
             System.out.println(this.elementos[i]);
             System.out.println(", ");
         }
+        System.out.println(this.capacidade);
     }
 
     public void contem(Integer valor) {
@@ -39,6 +43,36 @@ public class Vetor {
         }
         if (!achado) {
             System.out.println("O valor " + valor + " não está na lista");
+        }
+    }
+
+    public void remover(int posicao) {
+        if (posicao > this.tamanho) {
+            System.out.println("posição inválida");
+            return;
+        }
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+        }
+        this.elementos[this.tamanho - 1] = null;
+        this.tamanho--;
+    }
+    public void removerValor(int valor) {
+        boolean achado = false;
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i] == valor) {
+                achado = true;
+                for (int j = i; j < this.tamanho - 1; j++) {
+                    this.elementos[j] = this.elementos[j + 1];
+                }
+            }
+        }
+        if (achado) {
+            this.elementos[this.tamanho - 1] = null;
+        this.tamanho--;
+        } else {
+            System.out.println("nao tem esse valor");
+            return;
         }
     }
     
