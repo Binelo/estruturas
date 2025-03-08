dd07db984eb5d30c85ce387d348e06340a43b202
João Pedro Binelo <112009925+Binelo@users.noreply.github.com>
2025-03-07 21:53:26 -0300

    Add files via upload

--- adfas/vetores/VetorApp.java
+++ adfas/vetores/VetorApp.java
@@ -1,13 +1,17 @@
 public class VetorApp {
 
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(10);
+        Vetor vetor = new Vetor(7);
         vetor.adicionar(12);
         vetor.adicionar(10);
         vetor.adicionar(16);
         vetor.adicionar(4);
         vetor.adicionar(1);
         vetor.adicionar(18);
-        vetor.contem(40);
+        vetor.adicionar(19);
+        vetor.adicionar(192);
+        vetor.mostrarElementos();
+        // vetor.removerValor(16);
+        // vetor.mostrarElementos();
     }
 }
